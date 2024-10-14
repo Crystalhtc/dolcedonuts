5c22e43cf37beb93164717daecaa744f0b3ba735
Yae-Won Lee <ylee384@my.bcit.ca>
2024-10-14 16:51:56 -0700

    use NavLink to underline active page (Header)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'; 
 import styles from './Header.module.css';
 import logo from '../../assets/logo.svg'; 
 import cartIcon from '../../assets/cart-icon.svg'; 
@@ -7,22 +7,29 @@ export default function Header() {
   return (
     <nav className={styles.nav}>
       <ul className={styles.navList}>
-        <li className={styles.navItem}><Link to="/about">About</Link></li>
-        <li className={styles.navItem}><Link to="/menu">Menu</Link></li>
-        <li className={styles.logoContainer}>
-          <Link to="/">
-            <img src={logo} alt="Logo" className={styles.logo} />
-          </Link>
-        </li>
-        <li className={styles.navItem}><Link to="/order">Order</Link></li>
-        <li className={styles.navItem}><Link to="/contact">Contact</Link></li>
         <li className={styles.navItem}>
-          <Link to="/cart">
+          <NavLink to="/about" className={({ isActive }) => (isActive ? styles.activeLink : undefined)}>About</NavLink>
+        </li>
+        <li className={styles.navItem}>
+          <NavLink to="/menu" className={({ isActive }) => (isActive ? styles.activeLink : undefined)}>Menu</NavLink>
+        </li>
+        <li className={styles.logoContainer}>
+          <NavLink to="/">
+            <img src={logo} alt="Logo" className={styles.logo} />
+          </NavLink>
+        </li>
+        <li className={styles.navItem}>
+          <NavLink to="/order" className={({ isActive }) => (isActive ? styles.activeLink : undefined)}>Order</NavLink>
+        </li>
+        <li className={styles.navItem}>
+          <NavLink to="/contact" className={({ isActive }) => (isActive ? styles.activeLink : undefined)}>Contact</NavLink>
+        </li>
+        <li className={styles.navItem}>
+          <NavLink to="/cart" className={({ isActive }) => (isActive ? styles.activeLink : undefined)}>
             <img src={cartIcon} alt="Cart" className={styles.cartIcon} />
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </nav>
   );
 }
-
