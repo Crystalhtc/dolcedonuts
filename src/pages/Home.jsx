90b37c8a7cfe2d8b79da2be80c8c473d8dc90df7
Yae-Won Lee <ylee384@my.bcit.ca>
2024-10-14 18:34:16 -0700

    add banner image to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
+import React from 'react';
+import homeBanner from '../assets/home-main-banner.png';
+import styles from './Home.module.css'; 
+
 export default function Home() {
-    return(
-        <div>
-            home
+    return (
+        <div className={styles.homeContainer}>
+            <img 
+                src={homeBanner} 
+                alt="Home Main Banner" 
+                className={styles.homeBanner} 
+            />
         </div>
-    )
+    );
 }
