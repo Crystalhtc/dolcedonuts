6a0b1ea8b9abf1891a18d0d00e455fe4e5a0efa5
madoka <mnogaki@my.bcit.ca>
2024-11-15 22:04:09 -0800

    style cart preview for mobiles

--- src/components/CartPreview/CartPreview.jsx
+++ src/components/CartPreview/CartPreview.jsx
@@ -24,7 +24,7 @@ export default function CartPreview({ cart, cartSize, onClose }) {
             <div className={styles.modalContent} 
                 onClick={(e) => e.stopPropagation()}>
                 <div className={styles.heading}>
-                    <h2 className={styles.cartSizeText}>{cartSize} items</h2>
+                    <p>{cartSize} items</p>
                     <button className={styles.closeButton} onClick={onClose}>&times;</button>
                 </div>
                 <div className={styles.items}>
@@ -32,7 +32,7 @@ export default function CartPreview({ cart, cartSize, onClose }) {
                         <div key={product.id} className={styles.singleItem}>
                             <img src={product.image_url} alt={product.name} />
                             <div className={styles.info}>
-                                <p  className={styles.cartProductText}>{product.name}</p>
+                                <p className={styles.cartProductText}>{product.name}</p>
                                 <p>CA&nbsp; ${(product.price * quantity).toFixed(2)}</p>
                                 <p>{quantity}</p>
                             </div>
